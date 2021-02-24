93facbb45765f6b018f98bea4522bd83dabb3736
Namgyu Ho <itsnamgyu@gmail.com>
2021-02-24 19:06:25 +0900

    Project files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='reid-research',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='itsnamgyu',
+    author_email='',
+    description=''
+)
